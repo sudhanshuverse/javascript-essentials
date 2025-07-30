9f018ff8e5fee9cc83207eca9f921c4e5ec285ed
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-07-30 06:31:16 +0530

    Code Updated

--- 11-array-method-and-es6/script.js
+++ 11-array-method-and-es6/script.js
@@ -113,23 +113,39 @@
 
 // Spread Operator
 
-const num1 = [ 10, 20, 30, 40, 50 ];
-const num2 = [ 50, 40, 30, 20, 10 ];
+// const num1 = [ 10, 20, 30, 40, 50 ];
+// const num2 = [ 50, 40, 30, 20, 10 ];
 
-const joinArray = {...num1, ...num2};
-console.log(joinArray);
+// const joinArray = {...num1, ...num2};
+// console.log(joinArray);
 
 
 
-function add() {
+// function add() {
+//     let sum = 0;
+
+//     for(let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+// console.log(add(12,23,34,47,45,));
+// const arr = [10,20,30,40,50,60,70,80,90,100];
+// console.log(add(...arr));
+
+
+// Rest Parameters
+const nums1 = [1,2,3,4];
+
+function add(...nums) {
+    console.log('nums:', nums);
     let sum = 0;
-
-    for(let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+    for(let i = 0; i < nums.length; i++) {
+        sum = sum + nums[i];
     }
     return sum;
 }
 
-console.log(add(12,23,34,47,45,));
-const arr = [10,20,30,40,50,60,70,80,90,100];
-console.log(add(...arr));
+
+console.log( add( ...nums1 ));
