b528f34531c54f1ebd594a49eedacfdf2eca886d
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-03 21:03:42 +0530

    script.css uploaded

--- 13-js-dom-basics-examples/script.js
+++ 13-js-dom-basics-examples/script.js
@@ -1,29 +1,3 @@
-// < !-- < h1 > Difference between Element and Node</ >
-//     <p></p>
-//     <strong>Example</strong>
-//     <pre>
-//         <code></code>
-//     </pre> -->
-
-//     <h1>What isa append and appendChild</h1>
-//     <!-- < h1 > appendChild & append</ > -->
-//     <div class="container">
-//         <div class="card">1</div>
-//     </div>
-// const h1 = document.querySelector("h1")
-
-// const container = document.querySelector( ".container" )
-// const card = document.querySelector( '.card' )
-
-// const newCard = card.cloneNode();
-
-// for ( let i = 2; i <= 10; i++ ) {
-//     const newCard = card.cloneNode();
-//     newCard.innerText = i;
-//     container.appendChild(newCard);
-// }
-
-
 // 1. Element Selection
 function selectProduct () {
     const product = document.getElementById( "product" );
