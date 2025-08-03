6a35b27cfc946263910f26e4136c02b5fa6c5dc9
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-03 21:34:34 +0530

    script.css uploaded

---
+++ 14-node-vs-element-in-dom/script.js
@@ -0,0 +1,20 @@
+
+
+
+//     <h1>What isa append and appendChild</h1>
+//     <!-- < h1 > appendChild & append</ > -->
+//     <div class="container">
+//         <div class="card">1</div>
+//     </div>
+// const h1 = document.querySelector("h1")
+
+// const container = document.querySelector( ".container" )
+// const card = document.querySelector( '.card' )
+
+// const newCard = card.cloneNode();
+
+// for ( let i = 2; i <= 10; i++ ) {
+//     const newCard = card.cloneNode();
+//     newCard.innerText = i;
+//     container.appendChild(newCard);
+// }
