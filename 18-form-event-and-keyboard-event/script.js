43112b58152b7f0ae3700d587466f3e122570d00
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-04 19:17:30 +0530

    script.css uploaded

---
+++ 18-form-event-and-keyboard-event/script.js
@@ -0,0 +1,79 @@
+const userNameInput = document.querySelector('#username');
+const paragraph = document.querySelector('p');
+const form = document.querySelector('form');
+
+// userNameInput.addEventListener('click' , () =>{
+//     console.log("Single Click");
+// });
+
+
+// userNameInput.addEventListener( 'dblclick', () => {
+//     console.log( "Double Click" );
+// } );
+
+
+// Input Event
+// userNameInput.addEventListener( 'input', (e) => {
+//     console.log( e.target.value );
+//     paragraph.innerText = e.target.value;
+// } );
+
+
+// Change
+// userNameInput.addEventListener( 'change', (e) => {
+//     console.log( e.target.value );
+//     paragraph.innerText = e.target.value;
+// } );
+
+
+// Focus
+// userNameInput.addEventListener( 'focus', (e) => {
+//     console.log( e.target.value );
+//     paragraph.innerText = e.target.value;
+// } );
+
+
+// Blur
+// userNameInput.addEventListener( 'blur', ( e ) => {
+//     console.log( e.target.value );
+//     paragraph.innerText = e.target.value;
+// } );
+
+
+// form event
+// form.addEventListener( 'click', ( e ) => {
+//     const myFormData = new FormData(form);
+    
+//     for(const p of myFormData.entries()) {
+//         console.log(p);
+//     }
+
+//     console.log("Form Submit.");
+// } );
+
+
+
+
+// Key Press
+// const h1 = document.querySelector('h1');
+// window.addEventListener('keypress', (e) => {
+//     console.log("Key: ", e.key);
+//     console.log("Code: ", e.code);
+// } )
+
+// Key Up
+// const h1 = document.querySelector('h1');
+// window.addEventListener('keyup', (e) => {
+//     console.log("Key: ", e.key);
+//     console.log("Code: ", e.code);
+// } )
+
+
+// Key Down
+// const h1 = document.querySelector( 'h1' );
+// window.addEventListener( 'keydown', ( e ) => {
+//     console.log( "Key: ", e.key );
+//     console.log( "Code: ", e.code );
+// })
+
+
