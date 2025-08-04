9960b34098caf67fac1696327a8df2891a96f558
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-04 20:11:31 +0530

    script.css uploaded

---
+++ 19-mouse-touch-and-drag-event/script.js
@@ -0,0 +1,147 @@
+const h1 = document.querySelector( 'h1' )
+const card = document.querySelector( '.card' )
+const container = document.querySelector( '.container' )
+
+let count = 0
+
+// card.addEventListener( 'click', () => {
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+// Mouse Down
+// card.addEventListener( 'mousedown', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Mouse Up
+// card.addEventListener( 'mouseup', ( e ) => {
+//     console.log(e);
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Mouse Enter
+// card.addEventListener( 'mouseenter', ( e ) => {
+//     console.log(e);
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Mouse Leave Event
+// card.addEventListener( 'mouseleave', ( e ) => {
+//     console.log(e);
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Mouse Move
+// card.addEventListener( 'mouseleave', ( e ) => {
+//     console.log(e);
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Mouse Over
+// card.addEventListener( 'mouseover', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+
+
+// Wheel Event
+// card.addEventListener( 'wheel', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Scroll Event
+// card.addEventListener( 'scroll', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' )
+//     newCard.classList.add( 'card' )
+//     newCard.innerText = count++
+//     container.append( newCard )
+// } )
+
+
+// Touch Event
+// card.addEventListener( 'touchstart', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' );
+//     newCard.classList.add( 'card' );
+//     newCard.innerText = count++;
+//     container.append( newCard );
+// } )
+
+
+// Touch End
+// card.addEventListener( 'touchend', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' );
+//     newCard.classList.add( 'card' );
+//     newCard.innerText = count++;
+//     container.append( newCard );
+// } )
+
+
+// Touch move
+// card.addEventListener( 'touchmove', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' );
+//     newCard.classList.add( 'card' );
+//     newCard.innerText = count++;
+//     container.append( newCard );
+// } )
+
+
+// Touch Event
+// card.addEventListener( 'drag', ( e ) => {
+//     console.log( e );
+//     const newCard = document.createElement( 'div' );
+//     newCard.classList.add( 'card' );
+//     newCard.innerText = count++;
+//     container.append( newCard );
+// } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
