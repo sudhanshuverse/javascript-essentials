d351c41237db4f861af6afcd35c0fc080d198e0b
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-10-04 12:14:47 +0530

    updated

---
+++ 25-oop/script.js
@@ -0,0 +1,31 @@
+
+// function getAgeYear(){
+//     return new Date().getFullYear() - this.age
+// }
+
+
+// // Lecture 1
+
+// function createUser(firstName, lastName, age) {
+//     const user = {
+//         firstName,
+//         lastName,
+//         age,
+//         getAgeYear,
+
+//     }
+//     return user
+// }
+
+
+
+
+
+// function createUser(firstName, lastName, age){
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+// }
+
+// const user1 = new createUser("Sudhanshu", "Kumar", 20)
+// const user2 = new createUser("Himanshu", "Kumar", 18)
