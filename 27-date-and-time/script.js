a1e1867d4e6d5b0eb14c1acd830a623f50c56f5c
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-10-06 11:42:47 +0530

    Added

---
+++ 27-date-and-time/script.js
@@ -0,0 +1,42 @@
+const myDate = new Date()
+
+// GET TIME AND DATE IN DETAIL
+// console.log(myDate.getTime());
+
+// GET DATE
+// console.log(myDate.toLocaleDateString('en-GB'));
+
+// GET TIME
+// console.log(myDate.toLocaleTimeString('en-GB'));
+
+
+// console.log(myDate);
+// console.log(myDate.toLocaleDateString('en-GB'));
+// console.log(myDate.toLocaleTimeString('en-GB'));
+// console.log("Date: " + myDate.getDate());
+// console.log("UTC Date: " + myDate.getUTCDate());
+// console.log("Months: " + myDate.getMonth() + 1);
+// console.log("UTC Months: " + myDate.getUTCMonth() + 1);
+// console.log("Year: " + myDate.getFullYear());
+// console.log("UTC Year: " + myDate.getUTCFullYear());
+// console.log("Day: " + myDate.getDay());
+// console.log("UTC Day: " + myDate.getUTCDay());
+// console.log("Hours: " +myDate.getHours());
+// console.log("UTC Hours: " +myDate.getUTCHours());
+// console.log("Minutes: " +myDate.getMinutes());
+// console.log("UTC Minutes: " +myDate.getUTCMinutes());
+// console.log("Milliseconds: " + myDate.getMilliseconds());
+// console.log("UTC Milliseconds: " + myDate.getUTCMilliseconds());
+// console.log("Time Zone Offset: " + myDate.getTimezoneOffset() /-60 );
+// console.log("In JSON format: " + myDate.toJSON());
+
+console.log(myDate.toString());
+console.log(myDate.toUTCString());
+
+
+
+
+
+
+
+
